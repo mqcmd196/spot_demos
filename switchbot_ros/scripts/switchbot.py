1f6c8945625bedd680649e52542b9af34448514e
obinata <mqcmd196@hotmail.co.jp>
2020-12-10 13:00:14 +0900

    finish switchbot.py

--- switchbot_ros/scripts/switchbot.py
+++ switchbot_ros/scripts/switchbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import urllib.request
+from urllib.error import URLError, HTTPError
 import time
 
 class SwitchBotRequest:
@@ -15,9 +16,21 @@ class SwitchBotRequest:
 
     def request(self):
         req = urllib.request.Request(self.url)
-        with urllib.request.urlopen(req) as res:
-            self._body = res
-        self.status = self._body.status
+        try:
+            with urllib.request.urlopen(req) as res:
+                self._body = res
+                self.status = self._body.status
+                self.msg = self._body.msg
+        except HTTPError as e:
+            self._body = e
+            self.status = self._body.status
+            self.msg = self._body.msg
+            print('Got HTTPError. status:{} msg:{}'.format(e.code, e.msg))
+        except URLError as e:
+            self._body = e
+            self.status = None
+            self.msg = self._body.reason
+            print('Got URLError. reason:{}'.format(e.reason))
         
     def continuous_request(self, times, margin_sec):
        # add the least margin_sec
